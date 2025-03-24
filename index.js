44f21cbd966f51752faf31da3f87ad750527a2e6
Kshitij131 <1ms23cy032@msrit.edu>
2025-03-24 19:12:48 +0530

    Completed task 1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 
+const userName = process.argv[2];
+const userAge = parseInt(process.argv[3]);
+const isStudent = process.argv[4] === 'true';
+let toggleValue = false;
+function toggleBoolean() {
+    toggleValue = !toggleValue;
+    console.log(`Toggled value: ${toggleValue}`);
+  }
+
+
+function createMessage(name, age, student) {
+  let message = `Hello, my name is ${name}`;
+  if (student) {
+    message += ' and I am a student.';
+  }
+  if (process.argv[2] === 'toggle') {
+    toggleBoolean();
+    return;
+  }
+  if (age <= 10) {
+    let i=0;
+    while(i<age){
+        console.log(message);
+        i++;
+    }
+  } else {
+    console.log(message);
+  }
+}
+
+createMessage(userName, userAge, isStudent);
+
+
+
+
+
+
+
